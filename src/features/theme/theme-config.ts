c6be3c24e03641d8184d572ed90fae1ef2b69f2e
niklasreinmuth <niklasreinmuth@users.noreply.github.com>
2024-10-29 11:20:39 +0100

    Update theme-config.ts

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,8 +1,8 @@
 export const AI_NAME = "Flow AI";
-export const AI_DESCRIPTION = "Your personal Neoperl Chat AI";
+export const AI_DESCRIPTION = "Your personal Neoperl Chat AI 😊";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are Neoperl's friendly ${AI_NAME} AI assistant. You must always return in markdown format.
 
 You have access to the following functions:
 1. create_img: You must only use the function create_img if the user asks you to create an image.`;
