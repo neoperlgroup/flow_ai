bfd58efd13fb00832d32861fd48898b5b069c8c0
niklasreinmuth <niklasreinmuth@users.noreply.github.com>
2024-10-31 08:03:09 +0100

    [FA-0001] typo

--- src/features/chat-page/chat-input/chat-input.tsx
+++ src/features/chat-page/chat-input/chat-input.tsx
@@ -64,7 +64,7 @@ export const ChatInput = () => {
           }
         }}
         onKeyDown={(e) => {
-          onKeyDown(e, sumbit);
+          onKeyDown(e, submit);
         }}
         onKeyUp={(e) => {
           onKeyUp(e);
