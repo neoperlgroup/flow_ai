ba299d8d9c17c42af7c8b0e03712a0c9ce365493
niklasreinmuth <niklasreinmuth@users.noreply.github.com>
2024-11-04 19:55:07 +0100

    Update chat-text-input.tsx

--- src/features/ui/chat/chat-input-area/chat-text-input.tsx
+++ src/features/ui/chat/chat-input-area/chat-text-input.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+function clamp(num, min, max) {
+  return Math.min(Math.max(num, min), max);
+};
+
 export const ChatTextInput = React.forwardRef<
   HTMLTextAreaElement,
   React.TextareaHTMLAttributes<HTMLTextAreaElement> // Add ChatInputAreaProps to the type definition
 >(({ ...props }, ref) => {
+
+    React.useEffect(() => {
+      ref.current.addEventListener("input", (e) => {
+        ref.current.style.height = "auto";
+        ref.current.style.height = clamp(ref.current.scrollHeight, 40, 200) + "px";
+      })
+    });
+
   return (
     <textarea
       ref={ref}
