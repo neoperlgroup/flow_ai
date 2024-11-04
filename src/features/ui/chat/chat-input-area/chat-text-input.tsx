924a9bfbc22cfbcb37647d763770d5249880b41d
niklasreinmuth <niklasreinmuth@users.noreply.github.com>
2024-11-04 20:26:45 +0100

    Update chat-text-input.tsx

--- src/features/ui/chat/chat-input-area/chat-text-input.tsx
+++ src/features/ui/chat/chat-input-area/chat-text-input.tsx
@@ -1,25 +1,5 @@
 import React from "react";
 
-function clamp(num: number, min: number, max: number): number {
-  return Math.min(Math.max(num, min), max);
-}
-
-React.useEffect(() => {
-  const input = document.querySelector("#chatinput")
-  const handleInput = (e: Event) => {
-      if (!e.target) return;
-      e.target.style.height = "auto";
-      e.target.style.height = clamp(e.target.scrollHeight, 40, 200) + "px";
-  };
-
-  input.addEventListener("input", handleInput);
-
-  // Cleanup function to remove event listener
-  return () => {
-    input.removeEventListener("input", handleInput);
-  };
-}, []);
-
 export const ChatTextInput = React.forwardRef<
   HTMLTextAreaElement,
   React.TextareaHTMLAttributes<HTMLTextAreaElement> // Add ChatInputAreaProps to the type definition
@@ -28,7 +8,6 @@ export const ChatTextInput = React.forwardRef<
   return (
     <textarea
       ref={ref}
-      id="chatinput"
       className="p-4 w-full focus:outline-none bg-transparent resize-none "
       placeholder="Type your message here..."
       {...props}
